a9b68f481277075c3b349ce97f03fac9628e271c
William Kategianes <wKategianes@gmail.com>
2023-01-20 10:23:15 -0800

    Update main.js
    Created app's state variables

--- js/main.js
+++ js/main.js
@@ -2,7 +2,12 @@
 const AUDIO = new Audio('https://assets.mixkit.co/sfx/preview/mixkit-simple-countdown-922.mp3');
 
 /*----- app's state (variables) -----*/
-
+let scores; // Object key of "p" -> player; 
+            // key of "t" -> tie; 
+            // key of "c" -> computer
+let results; // Object key of "p" for the player result
+             // Values of "r" -> rock; "p" -> paper; "s" -> scissors;
+let winner; // String "p" if player wins; "t" for tie; "c" if computer wins
 
 /*----- cached element references -----*/
 
@@ -11,3 +16,26 @@ const AUDIO = new Audio('https://assets.mixkit.co/sfx/preview/mixkit-simple-coun
 
 
 /*----- functions -----*/
+init();
+
+// Initialize all state then call render();
+function init () {
+    scores = {
+        p: 0,
+        t: 0,
+        c: 0
+    };
+
+    results = {
+        p: "r",
+        c: "r"
+    };
+
+    winner = "t";
+
+    render();
+}
+
+function render() {
+    
+}
