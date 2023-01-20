610986aee72caed29be08be38a6aeee2e0d6c5c1
William Kategianes <wKategianes@gmail.com>
2023-01-20 10:11:40 -0800

    Initial Setup
    Created main.css file
    Created main.js file

---
+++ js/main.js
@@ -0,0 +1,13 @@
+/*----- constants -----*/
+const AUDIO = new Audio('https://assets.mixkit.co/sfx/preview/mixkit-simple-countdown-922.mp3');
+
+/*----- app's state (variables) -----*/
+
+
+/*----- cached element references -----*/
+
+
+/*----- event listeners -----*/
+
+
+/*----- functions -----*/
