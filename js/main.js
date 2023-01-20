7fe0c48c9cb304c2ff489e46d4d8a6b89c72bc26
William Kategianes <wKategianes@gmail.com>
2023-01-20 10:43:34 -0800

    Added render logic
    Added imgs folder
    Updated renderScores function
    Updated renderResults function

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 /*----- constants -----*/
 const AUDIO = new Audio('https://assets.mixkit.co/sfx/preview/mixkit-simple-countdown-922.mp3');
+const RPS_LOOKUP = {
+    r: "imgs/rock.png",
+    p: "imgs/paper.png",
+    s: "imgs/scissors.png"
+};
 
 /*----- app's state (variables) -----*/
 let scores; // Object key of "p" -> player; 
@@ -10,6 +15,8 @@ let results; // Object key of "p" for the player result
 let winner; // String "p" if player wins; "t" for tie; "c" if computer wins
 
 /*----- cached element references -----*/
+const pResultEl = document.getElementById("p-result");
+const cResultEl = document.getElementById("c-result");
 
 
 /*----- event listeners -----*/
@@ -36,6 +43,19 @@ function init () {
     render();
 }
 
+function renderScores() {
+    for (let key in scores) {
+        const scoreEl = document.getElementById(`${key}-score`);
+        scoreEl.innerText = scores[key]; // Square bracket notation used to dynamically access data
+    }
+}
+
+function renderResults() {
+    pResultEl.src = RPS_LOOKUP[results.p]; // Square bracket notation used to dynamiccaly access data
+    cResultEl.src = RPS_LOOKUP[results.c];
+}
+
 function render() {
-    
+    renderScores();
+    renderResults();
 }
